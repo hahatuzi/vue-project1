fc72034102c76782ef84f08ecdc008b75a54745b
user1 <user1@qq.com>
2020-04-13 15:56:10 +0800

    firstadd

---
+++ src/plugins/element.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import { Button, Container, Header, Aside, Main, Submenu, MenuItemGroup, MenuItem, Menu, Breadcrumb, Col, Row, Dialog, Pagination, Card, Input, Table, TableColumn } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Submenu)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Breadcrumb)
+Vue.use(Card)
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(Input)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Pagination)
+Vue.use(Dialog)
